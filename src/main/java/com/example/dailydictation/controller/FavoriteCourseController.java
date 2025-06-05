1cfef22a8764bc7c1d36403158a20da465215db3
Khoang Quan <your-email@example.com>
2025-06-05 23:20:58 +0700

    up

--- src/main/java/com/example/dailydictation/controller/FavoriteCourseController.java
+++ src/main/java/com/example/dailydictation/controller/FavoriteCourseController.java
@@ -10,15 +10,18 @@ import com.example.dailydictation.service.PracticeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
 public class FavoriteCourseController {
     @Autowired
     private FavoriteCourseService favoriteCourseService;
+
     @PostMapping("/create-favorite-course")
-    public ApiResponse<Void> createFavoriteCourse (@RequestBody FavoriteCourseRequest favoriteCourseRequest){
+    public ApiResponse<Void> createFavoriteCourse(@RequestBody FavoriteCourseRequest favoriteCourseRequest){
         favoriteCourseService.createFavoriteCourse(favoriteCourseRequest);
         return ApiResponse.<Void>builder()
                 .message("Success")
@@ -26,9 +29,26 @@ public class FavoriteCourseController {
     }
 
     @GetMapping("/show-all-favorite-course")
-    public ApiResponse<List<FavoriteCourseResponse>>showAllFavoriteCourse (@RequestParam int userId){
+    public ApiResponse<List<FavoriteCourseResponse>> showAllFavoriteCourse(@RequestParam int userId){
         return ApiResponse.<List<FavoriteCourseResponse>>builder()
                 .result(favoriteCourseService.showAllFavoriteCourse(userId))
                 .build();
     }
+
+    // API kiểm tra khóa học đã được yêu thích chưa
+    @GetMapping("/check-favorite")
+    public Map<String, Boolean> checkFavorite(@RequestParam int userId, @RequestParam int courseId) {
+        return Collections.singletonMap("isFavorite", favoriteCourseService.isFavorite(userId, courseId));
+    }
+
+
+    // API xóa khóa học khỏi danh sách yêu thích
+    @DeleteMapping("/delete-favorite-course")
+    public ApiResponse<Void> deleteFavorite(@RequestParam int userId, @RequestParam int courseId){
+        favoriteCourseService.deleteFavoriteCourse(userId, courseId);
+        return ApiResponse.<Void>builder()
+                .message("Deleted successfully")
+                .build();
+    }
 }
+
--- src/main/java/com/example/dailydictation/repository/FavoriteCourseRepository.java
+++ src/main/java/com/example/dailydictation/repository/FavoriteCourseRepository.java
@@ -5,9 +5,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface FavoriteCourseRepository extends JpaRepository<FavoriteCourse,Integer> {
-    boolean existsByCourseIdAndUserId(int courseId, int userId);
     List<FavoriteCourse>findAllByUserId(int userId);
+
+    Optional<FavoriteCourse> findByUserIdAndCourseId(int userId, int courseId);
+
+    boolean existsByUserIdAndCourseId(int userId, int courseId);
+
+    boolean existsByCourseIdAndUserId(int userId, int courseId);
 }
